05e4d8a3473a1966bc84063b26e37a624424fadb
SafalGhimire <58987042+SafalGhimire@users.noreply.github.com>
2020-01-11 20:50:51 +0545

    create and read

--- django_coursework/app/models.py
+++ django_coursework/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_Name = models.CharField(max_length=50)
+    last_Name = models.CharField(max_length=50)
+    Phoneno = models.IntegerField()
+    Address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.id) + " " + self.first_Name + " " +self.last_Name + " " + self.Address
